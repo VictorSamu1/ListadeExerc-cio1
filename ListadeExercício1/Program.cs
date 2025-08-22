0cd963394ed84d059b680b18e946f10473faa42b
VictorSamu1 <victorsamuelizaias@gmail.com>
2025-08-21 21:16:17 -0300

    Feito exercicío 1 da lista.

--- ListadeExercício1/Program.cs
+++ ListadeExercício1/Program.cs
@@ -1,14 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+Console.WriteLine("Cálculo da área de uma circunferência:");
 
+double raio, pi, area;
 
+pi = 3.14;
 
+Console.Write("Digite o valor do raio: ");
+raio = Double.Parse(Console.ReadLine());
 
+area = pi * (raio * raio);
 
-
-
-
-
-
-
+Console.WriteLine("O valor da área é de :" + area);
